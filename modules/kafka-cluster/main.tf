4da48f51c26249ae89318436d334ddb976066091
Jose F. Mingorance-Puga <jose@honestbank.com>
2022-08-29 14:15:28 +0700

    feat: revamp count into dynamic blocks

--- modules/kafka-cluster/main.tf
+++ modules/kafka-cluster/main.tf
@@ -1,32 +1,23 @@
 locals {
-  build_cluster_type_basic    = (var.cluster_for_production) ? 0 : 1
-  build_cluster_type_standard = (var.cluster_for_production) ? 1 : 0
+  build_cluster_type_basic    = var.cluster_for_production ? [] : ["yes"]
+  build_cluster_type_standard = var.cluster_for_production ? ["yes"] : []
 }
 
-resource "confluent_kafka_cluster" "basic" {
-  count = local.build_cluster_type_basic
-
+resource "confluent_kafka_cluster" "cluster" {
   display_name = var.kafka_cluster_name
   availability = var.availability
   cloud        = var.cloud
   region       = var.region
 
-  basic {}
-
-  environment {
-    id = var.environment_id
+  dynamic "basic" {
+    for_each = local.build_cluster_type_basic
+    content {}
   }
-}
 
-resource "confluent_kafka_cluster" "standard" {
-  count = local.build_cluster_type_standard
-
-  display_name = var.kafka_cluster_name
-  availability = var.availability
-  cloud        = var.cloud
-  region       = var.region
-
-  standard {}
+  dynamic "standard" {
+    for_each = local.build_cluster_type_basic
+    content {}
+  }
 
   environment {
     id = var.environment_id
