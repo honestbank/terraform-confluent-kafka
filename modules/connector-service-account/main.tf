e395a191c596943ed1e4d448383d65b6cd335358
Jose <jose.mingorance@gmail.com>
2023-03-10 15:05:34 +0700

    feat: add permissions to read topics into the 'connector-service-account' component

--- modules/connector-service-account/main.tf
+++ modules/connector-service-account/main.tf
@@ -1,3 +1,7 @@
+locals {
+  connector_service_account_principal = "User:${var.service_account_id}"
+}
+
 resource "confluent_kafka_acl" "connector_describe_on_cluster" {
   kafka_cluster {
     id = var.kafka_cluster_id
@@ -5,7 +9,7 @@ resource "confluent_kafka_acl" "connector_describe_on_cluster" {
   resource_type = "CLUSTER"
   resource_name = "kafka-cluster"
   pattern_type  = "LITERAL"
-  principal     = "User:${var.service_account_id}"
+  principal     = local.connector_service_account_principal
   host          = "*"
   operation     = "DESCRIBE"
   permission    = "ALLOW"
@@ -18,7 +22,7 @@ resource "confluent_kafka_acl" "connector_create_on_dlq_lcc_topics" {
   resource_type = "TOPIC"
   resource_name = "dlq-lcc"
   pattern_type  = "PREFIXED"
-  principal     = "User:${var.service_account_id}"
+  principal     = local.connector_service_account_principal
   host          = "*"
   operation     = "CREATE"
   permission    = "ALLOW"
@@ -31,7 +35,7 @@ resource "confluent_kafka_acl" "connector_write_on_dlq_lcc_topics" {
   resource_type = "TOPIC"
   resource_name = "dlq-lcc"
   pattern_type  = "PREFIXED"
-  principal     = "User:${var.service_account_id}"
+  principal     = local.connector_service_account_principal
   host          = "*"
   operation     = "WRITE"
   permission    = "ALLOW"
@@ -44,7 +48,22 @@ resource "confluent_kafka_acl" "app_connector_read_on_connect_lcc_group" {
   resource_type = "GROUP"
   resource_name = "connect-lcc"
   pattern_type  = "PREFIXED"
-  principal     = "User:${var.service_account_id}"
+  principal     = local.connector_service_account_principal
+  host          = "*"
+  operation     = "READ"
+  permission    = "ALLOW"
+}
+
+# See https://docs.confluent.io/cloud/current/connectors/service-account.html#example-configuring-a-service-account
+resource "confluent_kafka_acl" "connector_can_read_topics" {
+  for_each = toset(var.topics)
+  kafka_cluster {
+    id = var.kafka_cluster_id
+  }
+  resource_type = "TOPIC"
+  resource_name = each.value
+  pattern_type  = "LITERAL"
+  principal     = local.connector_service_account_principal
   host          = "*"
   operation     = "READ"
   permission    = "ALLOW"
