f1fcdd13d95b6ca3efca49b914a689216277af23
Sagar Trivedi <sagar@honestbank.com>
2024-11-29 09:54:33 +0700

    feat: Add feature to create metrics service account [DEVOP-5520]

--- modules/service-account/input.tf
+++ modules/service-account/input.tf
@@ -16,20 +16,36 @@ variable "service_account_name" {
 
 variable "cluster_id" {
   type        = string
-  description = "The ID of the Kafka cluster"
+  description = "The ID of the Kafka cluster. This value cannot be blank if `is_metrics_service_account` is set to `false`"
+  default     = null
 }
 
 variable "cluster_api_version" {
   type        = string
-  description = "API version of the Kafka cluster"
+  description = "API version of the Kafka cluster. This value cannot be blank if `is_metrics_service_account` is set to `false`"
+  default     = null
 }
 
 variable "cluster_kind" {
   type        = string
-  description = "The kind of the Kafka cluster"
+  description = "The kind of the Kafka cluster. This value cannot be blank if `is_metrics_service_account` is set to `false`"
+  default     = null
 }
 
 variable "environment_id" {
   type        = string
-  description = "The ID of the Confluent environment"
+  description = "The ID of the Confluent environment. This value cannot be blank if `is_metrics_service_account` is set to `false`"
+  default     = null
+}
+
+variable "is_metrics_service_account" {
+  type        = bool
+  description = "Set this value to true if you want to create a service account for metrics export else false"
+  default     = false
+}
+
+variable "cluster_crn" {
+  type        = string
+  description = "The Confluent Resource Name of the Kafka cluster, for example, `crn://confluent.cloud/organization=1111aaaa-11aa-11aa-11aa-111111aaaaaa/environment=env-abc123/cloud-cluster=lkc-abc123`. This value cannot be blank if `is_metrics_service_account` is set to `true`"
+  default     = null
 }
